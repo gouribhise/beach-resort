ff5684f7093cd4b2e6b236729760ea2df482f92d
gouri bhise <gouri.a.bhise@gmail.com>
2021-09-15 12:38:12 +0530

    added services section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
+import Services from '../components/Services'
 import {Link} from 'react-router-dom'
 
 function Home(){
@@ -11,6 +12,7 @@ function Home(){
             <Link to="/rooms" className="btn-primary"> our rooms</Link>
          </Banner>
       </Hero>
+      <Services/>
     </>
   )
 }
